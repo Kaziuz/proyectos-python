475b2c4fb21a3c9189ea1960fa7fb85df7812b26
johnny sepulveda <user.email>
2024-06-20 15:21:32 -0500

    add folder with proyect 'creador de nombres'

---
+++ 1.creador-de-nombres/creador_nombres.py
@@ -0,0 +1,7 @@
+'''
+Vamos a hacer un programa que pregunte al usuario algo y en base a eso se cree el nombre de una cerveza con el siguiente formato.
+La cerveza se llama: "respuesta1 respuesta2"
+'''
+
+print("Debes de escribir dos textos: uno indicando el nombre principal y el segundo el nombre secundario\n")
+print("La cerveza se llama:\n" + '"' + input("Cual es tu animal favorito: ") + " " + input("Cual es tu color favorito: ") + '"')
---
+++ 1.creador-de-nombres/readme.md
@@ -0,0 +1,4 @@
+# Creador de nombres
+Este código en Python pide al usuario responder dos preguntas que requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras combinadas, para formar una marca creativa.
+
+#
